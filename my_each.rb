c1dcf718511918676c62b5e52e804dd1c042c744
EJLarcs <ejlarcombe@gmail.com>
2015-07-31 17:27:19 -0400

    my_each

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,16 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+counter = 0
+  while counter < array.length 
+#length on an array tells you about of strings in array
+  yield array[counter]
+#yielding item in the array and then it comes back
+  counter += 1 
+  end
+  array
 end
+#when you have to puts from an array as well as return it you must
+#also call it at the end 
+
+#my_each(item) do |item|
+ # puts item 
+#end 
